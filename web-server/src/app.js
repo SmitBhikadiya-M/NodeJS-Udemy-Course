88f7395ca6f7394d4e4311fea9024ed9f1617b01
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-06-23 11:33:21 +0530

    Create HBS file structure for display dynamic views on browser

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 const fs = require('fs');
 
+// define path for express config
 const publicDir = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialPath = path.join(__dirname, '../templates/partials');
+
+// get instance of express
 const app = express();
 
+// set handlebars engine and views directory
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialPath);
+
+// set static directory to serve
 app.use(express.static(publicDir));
 
 app.get('', (req, res)=>{
     res.render('index', {
-        title: 'Weather App',
+        title: 'Home page',
         name: 'Smit Bhikadiya'
     });
 });
@@ -24,7 +35,8 @@ app.get('/about', (req, res)=>{
 
 app.get('/help', (req, res)=>{
     res.render('help', {
-        title: 'Help Page'
+        title: 'Help Page',
+        name: 'Ankit Chauhan'
     });
 });
 
