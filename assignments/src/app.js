d23b7b5afc88b6833c95179d09c3e5bc19db323b
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-06-27 16:17:58 +0530

    Update CORS policy configuration

--- assignments/src/app.js
+++ assignments/src/app.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 const corsOption = {
-    origin: '*'
+    origin: ['https://nodejs-udemy-assignments.herokuapp.com', 'http://localhost:3000/']
 };
 
 // defining path
