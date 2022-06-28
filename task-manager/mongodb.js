d86af0f7d8adb9c0afee3aaeb3f779e616cd793c
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-06-28 15:11:22 +0530

    Delete records from database (MongoDB): using deleteOne, deleteMany

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -9,30 +9,18 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client)=>{
    
    const db = client.db(databaseName);
 
-   // update records: update || updateMany && updateOne 
-   
-   // db.collection('users').updateOne({
-   //    _id: new ObjectID('62baa3be3b06e78e45c34efc')
-   // } , {
-   //    $set: {
-   //       name: "Shailesh"
-   //    },
-   //    $inc: {
-   //       age: 1
-   //    }
+   // deleting data from database: deleteOne, deleteMany
+
+   // db.collection("users").deleteOne({
+   //    age: 37
    // }).then((res)=>{
    //    console.log(res);
    // }).catch((err)=>{
    //    console.log(err);
    // });
 
-   db.collection('users').updateMany({ age: 27 }, {
-      $set: {
-         name: "Shahrukh"
-      },
-      $inc: {
-         age: 10
-      }
+   db.collection("users").deleteMany({
+      age: 37
    }).then((res)=>{
       console.log(res);
    }).catch((err)=>{
