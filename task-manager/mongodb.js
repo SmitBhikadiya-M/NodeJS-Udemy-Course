db8218147c05108d9a1d0a226ebce82ab5a7247f
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-06-28 15:05:59 +0530

    Update Users Data in mongoDB: updateMany, updateOne

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -9,6 +9,35 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client)=>{
    
    const db = client.db(databaseName);
 
+   // update records: update || updateMany && updateOne 
    
+   // db.collection('users').updateOne({
+   //    _id: new ObjectID('62baa3be3b06e78e45c34efc')
+   // } , {
+   //    $set: {
+   //       name: "Shailesh"
+   //    },
+   //    $inc: {
+   //       age: 1
+   //    }
+   // }).then((res)=>{
+   //    console.log(res);
+   // }).catch((err)=>{
+   //    console.log(err);
+   // });
+
+   db.collection('users').updateMany({ age: 27 }, {
+      $set: {
+         name: "Shahrukh"
+      },
+      $inc: {
+         age: 10
+      }
+   }).then((res)=>{
+      console.log(res);
+   }).catch((err)=>{
+      console.log(err);
+   });
+
 
 });
