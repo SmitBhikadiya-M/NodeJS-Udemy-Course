4e8ef4f1ccd363845cda1cb7f78e79568eb29268
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-06-29 11:21:32 +0530

    Coverting playground promises to async await format

--- task-manager/playground/promise-chaining-2.js
+++ task-manager/playground/promise-chaining-2.js
@@ -1,11 +1,23 @@
 require("../src/db/mongoose");
 const Task = require("../src/models/task");
 
-Task.findByIdAndDelete("62baf43309a60e134003426d", { completed: true })
-.then((res)=>{
-    return Task.countDocuments({ completed: false });
-}).then((result)=>{
-    console.log(result);
+// Task.findByIdAndDelete("62baf43309a60e134003426d", { completed: true })
+// .then((res)=>{
+//     return Task.countDocuments({ completed: false });
+// }).then((result)=>{
+//     console.log(result);
+// }).catch((err)=>{
+//     console.log(err);
+// });
+
+const deleteTaskAndCount = async (id,completed)=>{
+    await Task.findByIdAndDelete(id, {completed});
+    const count = await Task.countDocuments({completed});
+    return count;
+}
+
+deleteTaskAndCount("62bbe57421e0b648042355b4", true).then((res)=>{
+    console.log("count: "+res);
 }).catch((err)=>{
-    console.log(err);
+    console.log("err: "+err);
 });
--- task-manager/playground/promise-chaining.js
+++ task-manager/playground/promise-chaining.js
@@ -2,11 +2,23 @@ require("../src/db/mongoose");
 const User = require("../src/models/user");
 const user = require("../src/models/user");
 
-User.findByIdAndUpdate("62bad16225ca0dccc616b434", { age: 1 })
-  .then((user) => {
-    return User.countDocuments({ age: 1 });
-  }).then((result) => {
-    console.log(result);
-  }).catch((err) => {
+// User.findByIdAndUpdate("62bad16225ca0dccc616b434", { age: 1 })
+//   .then((user) => {
+//     return User.countDocuments({ age: 1 });
+//   }).then((result) => {
+//     console.log(result);
+//   }).catch((err) => {
+//     console.log(err);
+//   });
+
+  const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, {age});
+    const count = await User.countDocuments({age});
+    return count;
+  }
+
+  updateAgeAndCount("62bad6a3b51721d42d3620fb", 2).then((res)=>{
+    console.log("count: "+res);
+  }).catch((err)=>{
     console.log(err);
   });
