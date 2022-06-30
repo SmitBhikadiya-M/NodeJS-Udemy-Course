8b40c56b3b4b3424d75ed712b75f64de98abf2c2
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-06-30 16:16:29 +0530

    Added Pagination For Tasks list APIs

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -27,16 +27,27 @@ router.post('/tasks', auth, async (req, res)=>{
 // EndPoint: reading all tasks
 router.get("/tasks", auth, async (req, res)=>{
 
+    // filtering: GET users?completed=true
     const match = {}
-    console.log(req.query.completed);
     if(req.query.completed){
         match.completed = req.query.completed === 'true';
+    }
+
+    // pagination: GET users?limit=10&skip=0 ( show 10 records on first page )
+    const options = { limit: 2, skip: 0 }
+    if(req.query.limit){
+        options.limit = parseInt(req.query.limit);
+    }
+    if(req.query.page){
+        const pageNum = parseInt(req.query.page);
+        options.skip = (pageNum >= 1) ? (pageNum-1) * options.limit : 0; 
     }
 
     try{
         await req.user.populate({
             path: 'tasks',
-            match
+            match,
+            options
         }).execPopulate();
         res.send(req.user.tasks);
     }catch(e){
@@ -56,7 +67,7 @@ router.get("/tasks/:id", auth, async (req, res)=>{
 });
 
 // EndPoint: update user by its id
-router.patch('/tasks/:id', async (req, res)=>{
+router.patch('/tasks/:id', auth, async (req, res)=>{
 
     const id = req.params.id;
     const updates = Object.keys(req.body);
