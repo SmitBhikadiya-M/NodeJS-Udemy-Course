b75ba872a73841543135f5b63ec16c6edf02f721
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-06-30 15:14:19 +0530

    Attach middleware to delete all tasks whose associate with user when user is deleted

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -27,8 +27,8 @@ router.post('/tasks', auth, async (req, res)=>{
 // EndPoint: reading all tasks
 router.get("/tasks", auth, async (req, res)=>{
     try{
-        const tasks = await req.user.populate('tasks').execPopulate();
-        res.send(tasks);
+        await req.user.populate('tasks').execPopulate();
+        res.send(req.user.tasks);
     }catch(e){
         res.status(500).send(e);
     }
