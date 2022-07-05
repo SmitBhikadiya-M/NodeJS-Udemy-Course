2ed261db912cb426a7a389bc346dc81511dc4ffd
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-07-05 14:15:35 +0530

    Added Files and Folder and make some changed For Testing using JEST module

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,41 +1,43 @@
-const express = require('express');
-const supertest = require('supertest');
 
-const add = (a,b)=>{
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            if(a < 0 || b < 0){
-                reject("Accept only positive numbers");
-            }else{
-                resolve(a+b);
-            }
-        },2000);
-    });
-}
 
-test('Hello World!!!', ()=>{
-    expect(13).toBe(13);
-});
+// const add = (a,b)=>{
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//             if(a < 0 || b < 0){
+//                 reject("Accept only positive numbers");
+//             }else{
+//                 resolve(a+b);
+//             }
+//         },2000);
+//     });
+// }
 
-test('This should fail', ()=>{
-    throw new Error('I am failed!!');
-});
-
-// test('Async test demo', (done)=>{
-//     setTimeout(()=>{
-//         expect(1).toBe(2);
-//         done();
-//     },2000);
+// test('Hello World!!!', ()=>{
+//     expect(13).toBe(13);
 // });
 
-test('Should Add Two Number', (done)=>{
-    add(2,3).then((res)=>{
-        expect(res).toBe(9);
-        done()
-    });
-});
+// test('This should fail', ()=>{
+//     //throw new Error('I am failed!!');
+// });
 
-test('Should add two numbers async/await', async ()=>{
-    const sum = await add(10,22);
-    expect(sum).toBe(22);
+// // test('Async test demo', (done)=>{
+// //     setTimeout(()=>{
+// //         expect(1).toBe(2);
+// //         done();
+// //     },2000);
+// // });
+
+// test('Should Add Two Number', (done)=>{
+//     add(2,3).then((res)=>{
+//         expect(res).toBe(5);
+//         done()
+//     });
+// });
+
+// test('Should add two numbers async/await', async ()=>{
+//     const sum = await add(10,22);
+//     expect(sum).toBe(22);
+// });
+test('I am pass', ()=>{
+
 });
