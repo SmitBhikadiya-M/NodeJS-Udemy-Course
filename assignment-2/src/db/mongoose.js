484ef6d6fcd158df46f982506fff56f1f5b00a7a
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-07-05 18:06:22 +0530

    Added Router files for user and inventory

--- assignment-2/src/db/mongoose.js
+++ assignment-2/src/db/mongoose.js
@@ -2,3 +2,4 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URL);
 
 
+
--- assignment-2/src/index.js
+++ assignment-2/src/index.js
@@ -1,8 +1,13 @@
 require('./db/mongoose');
 const express = require('express');
+const userRouter = require('./routers/users');
+const inventoryRouter = require('./routers/inventory');
 
 const port = process.env.PORT;
 const app = express('/');
+
+app.use(userRouter);
+app.use(inventoryRouter);
 
 app.get('/', (req,res)=>{
     res.send("I am running..");
