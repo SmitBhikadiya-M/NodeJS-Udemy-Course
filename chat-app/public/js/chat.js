b76c6c5e9da453a46b6d33fec60ee8d073d071ac
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-07-12 15:48:28 +0530

    Done basic setup till message Template display on browser

---
+++ chat-app/public/js/chat.js
@@ -0,0 +1,69 @@
+const socket = io()
+const sendLocationBtn = document.querySelector('#send-location');
+const messageForm = document.querySelector('#message-form');
+const messages = document.querySelector('#messages');
+
+// Teplates
+const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationTemplate = document.querySelector("#location-template").innerHTML;
+
+
+socket.on('message', (message) => {
+    const html = Mustache.render(messageTemplate, { message });
+    messages.insertAdjacentHTML('beforebegin', html);
+});
+
+socket.on('location', (url)=>{
+    const html = Mustache.render(locationTemplate, { url });
+    messages.insertAdjacentHTML('beforeend', html);
+    sendLocationBtn.removeAttribute('disabled');
+})
+
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message, (error)=>{
+        if(error){
+            return console.log(error);
+        }
+        console.log("Message Delivered!!!");
+    })
+})
+
+sendLocationBtn.addEventListener('click', () => {
+
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.')
+    }
+
+    sendLocationBtn.setAttribute('disabled', 'disabled');
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, (error)=>{
+            if(error) return console.log(error);
+            console.log("Location Delivered!!!");
+            sendLocationBtn.removeAttribute('disabled');
+        });
+    }, (error) => {
+        sendLocationBtn.removeAttribute('disabled');
+        switch(error.code) {
+            case error.PERMISSION_DENIED:
+              console.log("User denied the request for Geolocation.")
+              break;
+            case error.POSITION_UNAVAILABLE:
+              console.log("Location information is unavailable.")
+              break;
+            case error.TIMEOUT:
+              console.log("The request to get user location timed out.")
+              break;
+            case error.UNKNOWN_ERROR:
+              console.log("An unknown error occurred.")
+              break;
+          }
+    });
+})
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,13 +1,48 @@
-const express = require("express");
-const path = require("path");
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+const Filter = require('bad-words');
 
-const app = express();
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
 const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-const publicDir = path.join(__dirname, '../public');
+app.use(express.static(publicDirectoryPath))
 
-app.use(express.static(publicDir));
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection');
 
-app.listen(port, ()=>{
-    console.log('Listening On post '+port);
-});
+    socket.emit('message', 'Welcome!');
+    socket.broadcast.emit('message', 'A new user has joined!');
+
+    socket.on('sendMessage', (message, callback) => {
+
+        const filter =new Filter();
+
+        if(filter.isProfane(message)){
+            return callback('Profanity is not allowed!');
+        }
+
+        io.emit('message', message);
+        callback();
+    });
+
+    socket.on('sendLocation', (coords, callback) => {
+        if(!coords.latitude || !coords.longitude){
+            return callback('Coordinates Not Found!!');
+        }
+        io.emit('location', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
+    });
+})
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`);
+})
