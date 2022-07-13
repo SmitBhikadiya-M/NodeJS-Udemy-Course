5c1821c10fd33e9e15a13b2b5e6a8a07509923b6
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-07-13 10:10:57 +0530

    Some Minor change

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -37,7 +37,7 @@ io.on('connection', (socket) => {
             return callback('Profanity is not allowed!');
         }
 
-        io.to().emit('message', generateMessage(message));
+        io.to('').emit('message', generateMessage(message));
         callback();
     });
 
