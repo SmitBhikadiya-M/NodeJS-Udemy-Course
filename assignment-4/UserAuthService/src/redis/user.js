ea8e57c03613b1b1c460f6132b5fc6f25ba337a5
Smit Bhikadiya <smit.bhikadiya@marutitech.com>
2022-07-22 14:12:08 +0530

    Create Test Suite for testing signup and signin REST

--- assignment-4/UserAuthService/src/redis/user.js
+++ assignment-4/UserAuthService/src/redis/user.js
@@ -10,8 +10,12 @@ const setUser = (user) => {
     return client.hSet('users', `users_${user.username}`, JSON.stringify({ ...user, requestCounter: 0 }));
 }
 
+const delUser = (username) => {
+    return client.hDel('users', `users_${username}`);
+}
+
 const connect = () => {
     return client.connect();
 };
 
-module.exports = { connect, getUser, setUser }
+module.exports = { connect, getUser, setUser, delUser }
